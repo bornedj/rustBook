c8502f04f3a20b1bef4abaea27a53b488af2b256
bornedj <bornedj@miamioh.edu>
2022-08-08 08:42:33 -0400

    finished the portion of the chapter on recoverable errors

--- error_handling/src/recoverable_errors.rs
+++ error_handling/src/recoverable_errors.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
-use std::io::ErrorKind;
+use std::fs::{self, File};
+use std::io::{self, ErrorKind, Read};
 
 pub fn recoverable_errors() {
 // will through an error
@@ -7,6 +7,18 @@ pub fn recoverable_errors() {
 kind();
 kind_methods();
 // error_unwrap()
+let username1 = read_username_from_file();
+let username2 = read_username_operator();
+let username3 = even_shorter_read_username();
+let username4 = shortest_read_username();
+println!(
+    "Username or error 1: {:?}, Username or error 2: {:?}, Username or error 3: {:?}, Username or error 4: {:?}",
+    username1,
+    username2, 
+    username3,
+    username4
+);
+//should all be the same we are reading from the same file without editing it
 println!("Recoverable_errors finished");
 }
 
@@ -61,3 +73,40 @@ fn kind_methods() {
 fn error_unwrap() {
     let f = File::open("hello.txt").unwrap();
 }
+
+//propagating the error aka passing the error back as a result
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// propagating is so common that rust provides the ? operator to make this easier
+fn read_username_operator() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn even_shorter_read_username() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn shortest_read_username() -> Result<String, io::Error> {
+    //fs provides a function that creates a new string, opens a file and writes its contents into a string if possible
+    fs::read_to_string("hello.txt")
+}
+// you can only implement the ? operator when the FromResidual trait is applied to the return type of the function
