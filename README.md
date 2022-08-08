04884f092e477c66593792a38e97ed42ec29bf38
bornedj <bornedj@miamioh.edu>
2022-08-07 23:41:50 -0400

    started the error handling chapters, got through most of 9.2

--- README.md
+++ README.md
@@ -2,35 +2,39 @@
 
 ## This repo is a collection of projects made while completing The [Rust Programming Language Book](https://doc.rust-lang.org/book)
 
-### Guessing_Game
+### guessing_game
 
 This project is a CLI number guessing game. It generates a random number from 1 to 100 (inclusive), and has the user guess until they have correctly guessed the number. The program will tell the user if the number they have provided is greater or less than the hidden random number.
 
-### Variables
+### variables
 
 Program explores rust compilers rules surrounding variables. Including scope, mutability, and data types.
 
-### Functions
+### functions
 
 Covers functions, expressions, and statements.
 
-### Ownership
+### ownership
 
 Covers concepts of the stack vs heap in memory allocation and how rust deals with the common pain points of other low-level languages. Covers references and borrowing, and introduces the concept of lifetimes. Also covers the slice type.
 
-### Structs
+### structs
 
 Covers defining and instantiating structs, as well as ownership and partially covers lifetimes. Covers methods, and associated functions within the rectangles project.
 
-### Enums
+### enums
 
 Covers defining enums, and there many use cases. How the match control flow can be useful with them. Also covers the if let syntax.
 
-### Restaurant
+### restaurant
 
 Project builds a restaurant library crate to demonstrate packages and modules.
 Chapter covers packages, crates, modules and use, paths, and public vs private within crate scope.
 
-### Collections
+### collections
 
 Covers the common standard library collections: strings, Vecs, and Hash Maps.
+
+### error_handling
+
+Covers the difference between unrecoverable and recoverable errors, how you should handle them, and when to expect the different kinds.
--- error_handling/src/recoverable_errors.rs
+++ error_handling/src/recoverable_errors.rs
@@ -1,3 +1,63 @@
-pub fn recoverable_errors() {
+use std::fs::File;
+use std::io::ErrorKind;
 
+pub fn recoverable_errors() {
+// will through an error
+// error_handling_with_match(); 
+kind();
+kind_methods();
+// error_unwrap()
+println!("Recoverable_errors finished");
+}
+
+
+fn error_handling_with_match() {
+    //File::open returns a result so we can expect a recoverable error
+    let f = File::open("hello.txt");
+
+    // error handling with match
+    let f = match f {
+        Ok(file) => file,
+        // Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => panic!("Problem opening the file: {:?}", error), 
+    };
+    // shows a notfound file error 
+}
+
+// we can handle an error by matching its variant with kind()
+fn kind() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {// need the std::io::ErrorKind for this
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+}
+
+// different syntax with the methods around results to get same function as kind
+// uses closures
+fn kind_methods() {
+  let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
+
+
+//you can also use unwrap
+fn error_unwrap() {
+    let f = File::open("hello.txt").unwrap();
 }
