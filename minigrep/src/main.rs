4bb5afc834869fac847ad352f0bcb886baf4a243
bornedj <bornedj@miamioh.edu>
2022-08-09 17:22:05 -0400

    fixed compiler issue by adding [lib] key in toml.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,13 +7,13 @@ fn main() {
     // creating references to our args
     // let config = parse_config(&args);
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     // reading contents
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
