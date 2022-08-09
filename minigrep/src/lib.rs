bccdd56ddef3345df3a93a38d04f921494ee6020
bornedj <bornedj@miamioh.edu>
2022-08-09 11:39:25 -0400

    finished error handling for run, adding tests

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,36 @@
+use std::error::Error;
+use std::fs;
+// CLI parsing
+// depreciated because of the config constructor
+// fn parse_config(args: &[String]) -> Config {
+//     let query = args[1].clone();
+//     let filename = args[2].clone();
+//     Config {query, filename}
+// }
+
+pub struct Config {
+    pub query: String,
+    pub filename: String
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments.");
+        }
+        let query = args[1].clone(); 
+        let filename = args[2].clone(); 
+        Ok(Config {query, filename})
+    }
+}
+
+// function takes the config and reads the file with the appropriate name
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("With text:\n{}", contents);
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
 
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,5 @@
-// #![allow(dead_code)]
-pub use std::{env, fs, process, error::Error};
+use std::{env, process};
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();// return the args as vector of strings
@@ -14,39 +14,8 @@ fn main() {
     println!("In file {}", config.filename);
 
     // reading contents
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-// CLI parsing
-// depreciated because of the config constructor
-// fn parse_config(args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let filename = args[2].clone();
-//     Config {query, filename}
-// }
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments.");
-        }
-        let query = args[1].clone(); 
-        let filename = args[2].clone(); 
-        Ok(Config {query, filename})
-    }
-}
-
-// function takes the config and reads the file with the appropriate name
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
-    Ok(())
 }
