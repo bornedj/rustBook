6ca2c71767390e2c9992d98fc70b6c03992a32c8
bornedj <bornedj@miamioh.edu>
2022-08-20 14:07:50 -0400

    finished section on concurrency with shared state

--- concurrency/src/lib.rs
+++ concurrency/src/lib.rs
@@ -1,6 +1,6 @@
 use std::thread; 
 use std::time::Duration;
-use std::sync::mpsc;
+use std::sync::{mpsc, Mutex, Arc};
 
 pub fn intro_concurrency() {
     // thread will not finish it's execution before the main thread and as such will not finish its execution
@@ -120,6 +120,38 @@ pub fn multiple_producer_single_consumer () {
         println!("Got: {}", received);
     }
 
+}
+
+pub fn example_mutex () {
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();// lock blocks the thread from doing work until we have ownership of the lock
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+}
+
+pub fn sharing_mutex_btw_threads () {
+    let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
+
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+            let handle = thread::spawn(move || {
+                let mut num = counter.lock().unwrap();
+
+                *num += 1;
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        println!("Result: {}", *counter.lock().unwrap());
 }
 
 #[cfg(test)]
--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -15,6 +15,12 @@ fn main () {
     println!("\nChannel showing concurrency as well");
     concurrency::channel_display_concurrency();
 
-    println!("\n Function showing how multiple producers can pass inputs to a single consumer thread");
+    println!("\nFunction showing how multiple producers can pass inputs to a single consumer thread");
     concurrency::multiple_producer_single_consumer();
+
+    println!("\nMoving into shared state concurrency. Demonstration of mutex.");
+    concurrency::example_mutex();
+
+    println!("\nSharing state between threads using atomic reference counts and mutexes");
+    concurrency::sharing_mutex_btw_threads();
 }
