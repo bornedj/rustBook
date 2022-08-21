f1a876809da71358b90eac3ca142e0c99c7d2910
bornedj <bornedj@miamioh.edu>
2022-08-21 10:27:35 -0400

    implemented a split_at_mut function to demonstrate how rusts library builds the function

--- advanced_rust/src/lib.rs
+++ advanced_rust/src/lib.rs
@@ -1,10 +1,58 @@
 #![allow(unused_variables)]
+use std::slice;
 pub fn raw_pointers () {
     // you can declare raw pointers outside of unsafe rust blocks, but they cannot be dereferenced outside of an unsafe block
     let mut num = 5;
 
     let r1 = &num as *const i32;
     let r2 = &mut num as *mut i32;
+}
+
+pub fn raw_pointer_arbitrary () {
+    let address = 0x012345usize;
+    let r = address as *const i32;
+}
+
+pub fn deref_raw_pointer () {
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+}
+
+pub unsafe fn dangerous() {
+    println!("Running an unsafe function");
+}
+
+pub fn using_split_at_mut () {
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+}
+
+
+pub fn my_split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
 }
 
 #[cfg(test)]
--- advanced_rust/src/main.rs
+++ advanced_rust/src/main.rs
@@ -1,4 +1,18 @@
 use advanced_rust;
 fn main () {
     advanced_rust::raw_pointers();// declaration of raw pointers outside of an unsafe block
+    advanced_rust::raw_pointer_arbitrary(); // declares a raw pointer at an arbitrary memory location. Just a demonstration that this is possible, there are extremely few cases where you would chose to do this
+    advanced_rust::deref_raw_pointer(); // uses an unsafe block to deref a raw pointer, and print the value 
+    // dangerous does nothing, but is an unsafe function. Used to demonstrate that there has to be a unsafe block to call the function
+    unsafe {
+        advanced_rust::dangerous();
+    }
+    advanced_rust::using_split_at_mut(); // split_at_mut is a standard method of Vecs that splits a vector at a given index, and must be written using unsafe rust code
+    // function must be written using unsafe rust as it makes to mutable borrows of a vector
+    
+    // my_split_at_mut is a demonstration of how the split_at_mut fn is implemented, asserts show it splits a mut ref to an array into two mut refs of the array by a given index
+    let values = &mut [1,2,3,4];
+    let (a, b) = advanced_rust::my_split_at_mut(values, 2);
+    assert_eq!(a, &mut [1,2]);
+    assert_eq!(b, &mut [3,4]);
 }
